1c63020aaa57a14d49e93723898c20b4ef3327f9
Ramaneek Gill <ramaneek.gill@hotmail.com>
2015-08-18 19:52:59 -0400

    Extract hashtags from dataset

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -9,10 +9,24 @@ def readCsv(filename):
 		dataset[line] = [str(i) for i in dataset[line]]
 	return dataset
 
+def extractTweets(dataset, tweetIndex, maxNumHashtags):
+	copy = list(dataset)
+	hashtagSet = list()
+	for line in copy:
+		for word in line[tweetIndex].split():
+			if word.startswith('#'):
+				hashtagSet.insert(-1, [word, copy.index(line)])
+				line[tweetIndex].replace(word, "")
+				if len(hashtagSet) == maxNumHashtags:
+					return hashtagSet, copy
+
+	return hashtagSet, copy
+
 
 def main():
 	filename = 'testdata.manual.2009.06.14.csv'
 	dataset = readCsv(filename)
-	print(dataset[0])
+	hashtagSet, hashtagExtractedDataset = extractTweets(dataset, -1, 2)
+	print(hashtagSet)
 
 main()
