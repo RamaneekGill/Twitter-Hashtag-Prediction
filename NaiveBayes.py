f73567a685855e8e3a03b0ac73cec5f705cfbc8c
Ramaneek Gill <ramaneek.gill@hotmail.com>
2015-08-18 21:30:11 -0400

    Refactor extractTweets(); Split the dataset into training and test

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -10,7 +10,8 @@ def readCsv(filename):
 		dataset[line] = [str(i) for i in dataset[line]]
 	return dataset
 
-# Finds hashtags in a tweet
+
+# Finds hashtags in a tweet and extracts them
 # Also returns a dataset of tweets that don't contain hashtags
 def extractTweets(corpus, tweetIndex, maxNumHashtags=sys.maxsize):
 	hashtagSet = list()
@@ -25,17 +26,36 @@ def extractTweets(corpus, tweetIndex, maxNumHashtags=sys.maxsize):
 
 		if len(tempHashtagSet) > 0:
 			hashtagSet.append(tempHashtagSet)
+
+			# Remove the hastag from this tweet
+			for word in tempHashtagSet:
+				line[tweetIndex] = line[tweetIndex].replace(word, "")
 			dataset.append(line[tweetIndex])
 
 	return hashtagSet, dataset
 
+
+def seperateDatasetInTwo(dataset, ratio):
+	trainSize = int(len(dataset) * ratio)
+	trainSet = list()
+	testSet = list(dataset)
+	while len(trainSet) < trainSize:
+		index = random.randrange(len(testSet))
+		trainSet.append(testSet.pop(index))
+	return [trainSet, testSet]
+
+
 ###############################  MAIN  #########################################
+
 
 def main():
 	filename = 'testdata.manual.2009.06.14.csv'
+	# filename = 'training.1600000.processed.noemoticon.csv'
 	corpus = readCsv(filename)
 	hashtagSet, dataset = extractTweets(corpus, -1, 50)
-	print(hashtagSet)
-	print(len(hashtagSet), len(dataset), len(corpus))
+	trainSet, testSet = seperateDatasetInTwo(dataset, 0.8)
+	print(trainSet)
+	print('\n\n\n')
+	print(testSet)
 
 main()
