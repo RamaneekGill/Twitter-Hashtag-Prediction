88c61df868c71de446b5074c96f5e2f6ee6fb889
Ramaneek Gill <ramaneek.gill@hotmail.com>
2015-12-12 17:38:20 -0500

    Test out different cost functions

--- LogisticRegression/LogisticRegression.py
+++ LogisticRegression/LogisticRegression.py
@@ -31,7 +31,7 @@ def main():
 	# data contains the 3 different sets, Each a DataSet class
 	numWords, numHashtags, data = input_data.read_data_sets()
 
-	LEARNING_RATE = 0.1
+	LEARNING_RATE = 0.05
 	EPOCHS = 200
 	BATCH_SIZE = 1000
 	DISPLAY_STEP = 1 # To print cost every n number of epochs
@@ -46,12 +46,12 @@ def main():
 	# Setup the model
 	W = tf.Variable(tf.random_normal([numWords, numHashtags], stddev=1)) # weights matrix
 	b = tf.Variable(tf.zeros([numHashtags])) # bias
-	activation = tf.nn.sigmoid(tf.matmul(x,W) + b) # the predictions
-	# choose between sigmoid and softmax
+	activation = tf.nn.softmax(tf.matmul(x,W) + b) # the predictions, choose between sigmoid and softmax
 
 	# Specify the cost and optimization
 	cost = -tf.reduce_sum(y*tf.log(activation)) # cross entropy is the cost function
-	cost = tf.reduce_sum(tf.square(y - activation))
+	# cost = tf.reduce_sum(tf.square(y - activation))
+	# cost = tf.reduce_sum(y*activation + (1-y)*(1-activation))
 	optimizer = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(cost)
 
 	# Initialize session and its variables
@@ -70,6 +70,8 @@ def main():
 
 	validation_accuracies = []
 
+	print("TRAIN \t VALID \t TEST \t COST \t EPOCH")
+
 	for epoch in range(EPOCHS):
 		avg_cost = 0.0
 		num_batches = int(data.train_set.num_examples() / BATCH_SIZE)
@@ -80,14 +82,22 @@ def main():
 			avg_cost += sess.run(cost, feed_dict={x: batch_xs, y: batch_ys}) / num_batches
 
 		if epoch % DISPLAY_STEP == 0:
-			print "Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost)
 
-			# Test it against the validation set
-			print("Testing against validation set")
-			correct_prediction = tf.equal(tf.argmax(activation, 1), tf.argmax(y, 1))
-			accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-			print("Accuracy on validation set predicting 1 hashtag: {}".format(sess.run(accuracy, feed_dict={x: data.validation_set.inputs(), y: data.validation_set.targets()})))
+			# Training Set Accuracy
+			num_correct = 0
+			predictions = sess.run(activation, feed_dict={x: data.train_set.inputs()})
+			targets = data.train_set.targets()
+			for i in range(len(predictions)):
+				top_prediction_indexes = predictions[i].argsort()[-PREDICTION_RANGE:][::-1]
 
+				for index in top_prediction_indexes:
+					if targets[i][index]:
+						num_correct += 1
+						break
+
+			train_accuracy = float(num_correct) / float(len(predictions))
+
+			# Validation Set Accuracy
 			num_correct = 0
 			predictions = sess.run(activation, feed_dict={x: data.validation_set.inputs()})
 			targets = data.validation_set.targets()
@@ -101,9 +111,8 @@ def main():
 
 			validation_accuracy = float(num_correct) / float(len(predictions))
 			validation_accuracies.append(validation_accuracy)
-			print("Accuracy on validation set prediction out of 5 hashtags: {}".format(validation_accuracy))
 
-			# Just for kicks print out accuracy on test set
+			# Test Set Accuracy
 			num_correct = 0
 			predictions = sess.run(activation, feed_dict={x: data.test_set.inputs()})
 			targets = data.test_set.targets()
@@ -115,8 +124,14 @@ def main():
 						num_correct += 1
 						break
 
-			percentage = float(num_correct) / float(len(predictions))
-			print("Accuracy of model prediction {} hashtags: num correct = {}, out of {}, percentage = {}".format(PREDICTION_RANGE, num_correct, len(predictions), percentage))
+			test_accuracy = float(num_correct) / float(len(predictions))
+
+			print("{:.4f} \t {:.4f} \t {:.4f} \t {:.4f} \t {}".format(train_accuracy,
+				validation_accuracy,
+				test_accuracy,
+				avg_cost,
+				epoch))
+
 
 		# If performance is getting worse stop training
 		if validation_accuracy < max(validation_accuracies) and epoch > 5:
@@ -147,8 +162,8 @@ def main():
 	print(validation_accuracies)
 
 	# Save the model
-	print("Saving weights")
-	save_path = saver.save(sess, "mymodel")
-	print "Model saved in file: ", save_path
+	# print("Saving weights")
+	# save_path = saver.save(sess, "mymodel")
+	# print "Model saved in file: ", save_path
 
 main()
